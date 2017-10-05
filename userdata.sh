237dec2ab0d65129d5b15eb1eb8382e77fa934de
James Kwok <jameskwok@L-C02V81BCHV2J-James-Kwok.local>
2017-10-05 11:31:32 +1100

    add node exporter

--- userdata.sh
+++ userdata.sh
@@ -5,8 +5,22 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
+# Install node exporter to collect system metrics for Prometheus
+apt-get install -y prometheus-node-exporter
+#Create Prometheus Target node-exporter runs on port 9100
+cat <<EOF > prometheus.yml
+global:
+  scrape_interval:     10s
+  evaluation_interval: 10s
+
+scrape_configs:
+  - job_name: 'prometheus'
+    static_configs:
+      - targets: ['localhost:9090', 'localhost:9100']
+
+EOF
 #Run Prometheus
-docker run -d -p 9090:9090 prom/prometheus
+docker run -d -p 9090:9090 -v /prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
 #Run Grafana
 mkdir -p /var/lib/grafana
 docker run -d -v /var/lib/grafana --name grafana-storage busybox:latest
