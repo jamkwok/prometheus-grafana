75e958fcd470c773fac9c9d71e56b4ccc891484d
James Kwok <jameskwok@L-C02V81BCHV2J-James-Kwok.local>
2017-10-05 12:05:06 +1100

    30s imeout

--- userdata.sh
+++ userdata.sh
@@ -8,10 +8,12 @@ apt-get install -y docker-ce
 # Install node exporter to collect system metrics for Prometheus
 apt-get install -y prometheus-node-exporter
 #Create Prometheus Target node-exporter runs on port 9100
+# Timeout raised to 30s due to docker prometheus issue
 cat <<EOF > prometheus.yml
 global:
-  scrape_interval:     10s
-  evaluation_interval: 10s
+  scrape_interval:     30s
+  evaluation_interval: 30s
+  scrape_timeout: 30s
 
 scrape_configs:
   - job_name: 'prometheus'
